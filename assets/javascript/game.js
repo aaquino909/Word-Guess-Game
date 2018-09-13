10b8a36d6c82d050001e8809d6bf6307f64ea4e9
Angelo Aquino <aaquino909@gmail.com>
2018-09-13 14:38:31 -0700

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,32 +5,27 @@
     var win = 0;
     var lose = 0;
     var guessesLeft = 15; //total of guesses a user can have
-    var list = ["orange", "apple", "pineapple", "peach", "strawberry", "pear", "grape", "avocado","mango","blueberry", "cherry"]; 
+    var list = ["orange", "apple", "pineapple", "peach", "strawberry", "pear", "grape", "avocado","mango","blueberry", "cherry", "papaya", "watermelon", "kiwi", "fig", "guava"]; 
     var guessedLetters = []; //array push user's guesses
-    
-    
-    var randomIndex = Math.floor(Math.random()*11);
+
+    var randomIndex = Math.floor(Math.random()*list.length);
     var chosenWord = list[randomIndex]; //random index inside list assigned to chosenWord
     console.log(chosenWord);
     var underscore= []; //empty string for underscore
-    
-    
-    //STEP TWO: use a for loop to display amount of underscores using wordLength;
-    function start(){ //function to start forloop and display underscore
+
+
+    //STEP TWO: use a for loop to display amount of underscores using wordLength; //function to start forloop and display underscore
     for (var i = 0; i < chosenWord.length; i++ ){
         underscore[i]= "_ "; //keeps adding underscore to the amount of letters fruit
-    }
+        }
     document.getElementById("underscore").innerHTML= underscore.join("") ;
-    }
-    start(); //starts function
-    
+ 
 
 
     //Function to match users guessed letter to each index of array
     function matchWithUsers() {
         var letterPosition = chosenWord.split(""); //turns chosenWord into an array and puts into letterPosition
         var userGuess = event.key; //"a" goes into user guess
-
     for (var i = 0; i <  letterPosition.length; i++){ //for as long as the array
         if (letterPosition[i] === userGuess){  // if index i is equal to the userguess
             // then put userguess letter and replace it with matching underscore array 
@@ -42,7 +37,6 @@
     }
 
 
-    
     document.onkeyup = function(event) { //function to execute for each key Press
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
         guessedLetters.push(userGuess); //adds ever letter pushed to guessedLetters array
@@ -57,25 +51,20 @@
             guessedLetters = [];
             document.getElementById("lose").innerHTML = lose;
             document.getElementById("guessedLetters").innerHTML = guessedLetters.join("  ");
-            document.getElementById("guessesLeft").innerHTML = guessesLeft;   
+            document.getElementById("guessesLeft").innerHTML = guessesLeft;
         }
         if (underscore.join("") === chosenWord){
             alert("You're a winner Baby! The word was " + chosenWord.toUpperCase());
             win++;
             guessesLeft = 15;
             guessedLetters = [];
-            underscore = [];
-            for (var i = 0; i < chosenWord.length; i++ ){
-                underscore[i]= "_ "; //keeps adding underscore to the amount of letters fruit
-            }
-            document.getElementById("underscore").innerHTML= underscore.join("") ;
-            
-            console.log("underscore" + underscore);
-            console.log("guessedLetters" +guessedLetters);
-            console.log("chosenWord" + chosenWord);
             document.getElementById("win").innerHTML= win;
             document.getElementById("guessedLetters").innerHTML = guessedLetters.join("  ");
             document.getElementById("guessesLeft").innerHTML = guessesLeft;
+            // location.reload();
         }
+        
+        
     }
+
 
